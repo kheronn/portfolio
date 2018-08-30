839d90653ad415913e54a7d33643b99512b216b2
Kheronn <kheronn@gmail.com>
2018-08-30 15:38:22 -0300

    Update config firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyC7iFlcrvM0dzjfoDqEbLRHsSskJIWHyLU",
-    authDomain: "portifolio-web.firebaseapp.com",
-    databaseURL: "https://portifolio-web.firebaseio.com",
-    projectId: "portifolio-web",
-    storageBucket: "portifolio-web.appspot.com",
-    messagingSenderId: "8131957317"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
 
   }
 };
